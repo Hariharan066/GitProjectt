5a624c34022d9d08d6e0e0ea74cf9d6dad70382f
Hariharan066 <crazyharichutty@gmail.com>
2023-09-13 11:56:08 +0530

    hari code

--- src/test/java/org/test/TestClass.java
+++ src/test/java/org/test/TestClass.java
@@ -1,5 +1,11 @@
 package org.test;
 
 public class TestClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Hari");
+		System.out.println("Haran");
+		System.out.println("it is my name");
+	}
 
 }
